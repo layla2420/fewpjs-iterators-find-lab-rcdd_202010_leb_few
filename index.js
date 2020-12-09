53c9d5568e68e6c5a8b3e0f129bcb5cb5fac6ca1
layla  hammoud <laylahammoud63@gmail.com>
2020-12-09 11:19:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ function testFunc() {
 superbowlWin = (record) => {
   let result = record.find( record => record.result === "W" )
   return result.year
-} 
+}
   return !!result ? result.year : undefined
 }
