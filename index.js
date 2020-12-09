de8fb27cc5ede60aed938f3b253caae740d3f93e
layla  hammoud <laylahammoud63@gmail.com>
2020-12-09 11:13:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+function superbowlWin(record){
+let result=record.find(record=>record.result==="w")
+  return !!result ? result.year : undefined
+}
